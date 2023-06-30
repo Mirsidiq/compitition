d5066abe0cbf7d8a6dac972e5c6ddc47576da2d7
Mirsidiq <mirzokirov2002@gmail.com>
2023-06-30 20:07:31 +0500

    open users route

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,7 @@
 import auth from "./auth/router.js";
 import directions from "./directions/router.js";
 import groups from "./groups/router.js";
-// import users from "./users/router.js";
+import users from "./users/router.js";
 import assistents from "./assistents/router.js"
 import students from "./students/router.js"
 import homeworks from "./homeworks/router.js"
@@ -13,4 +13,5 @@ export default [
   assistents,
   students,
   homeworks,
+  users
 ];
